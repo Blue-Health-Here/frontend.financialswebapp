4b50310fee4eae483da1ff89005c03f9ec57d687
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-02-27 19:41:28 +0500

    change metadata of the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Financials Web App",
+  description: "The financials web app for all your financial needs.",
 };
 
 const geistSans = Geist({
