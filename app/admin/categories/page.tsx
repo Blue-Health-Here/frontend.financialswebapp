cbef7571c51e822a23308bd647daa89afe686cb3
Mehreen47 <mehreen@bluehealthhere.com>
2025-03-04 02:45:36 +0500

    Design the admin categories page
    create a category content component for displaying the categories

---
+++ app/admin/categories/page.tsx
@@ -0,0 +1,14 @@
+
+
+import AdminLayout from "@/components/layouts/AdminLayout";
+import CategoryContent from "@/components/common/CategoryContent";
+
+export default function Categories() {
+
+    return (
+        <AdminLayout>
+            <CategoryContent />
+        </AdminLayout>
+    );
+}
+
