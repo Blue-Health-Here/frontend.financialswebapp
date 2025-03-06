084409e56ccc86a559e1af7c15b70d9b1d17e04f
Mehreen47 <mehreen@bluehealthhere.com>
2025-03-06 18:41:33 +0500

    Design New Marketing Material sidebar popup form

--- components/admin/marketing/MarketingSection.tsx
+++ components/admin/marketing/MarketingSection.tsx
@@ -1,15 +1,28 @@
+"use client"
+
 import React from 'react'
 import { Input } from "@/components/ui/input";
 import { IoSearch } from "react-icons/io5";
 import InfoCard from "@/components/common/InfoCard";
-import { Button } from "@/components/ui/button";
 import { marketinMaterial } from '@/utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
+import AddMarketingModal from './AddMarketingModal';
+import { setIsAddMarketing } from '@/store/features/marketing/marketingSlice';
+import { SubmitButton } from '@/components/submit-button';
+import { FaPlus } from "react-icons/fa";
+
 const MarketingSection = () => {
+    const { isAddMarketing } = useSelector((state: RootState) => state.marketing)
+    const dispatch = useDispatch()
+    const handleAddMarketing = () => {
+        dispatch(setIsAddMarketing(true))
+    }
     return (
         <div className="p-6 pt-8 pb-9 bg-white shadow-lg rounded-lg">
             <div className="flex items-center justify-between flex-wrap gap-4 pb-6">
                 <div className="flex items-center justify-between gap-3">
-                    <h1>Marketing Material</h1><Button className="w-6 h-7 text-2xl text-white pb-2.5">+</Button>
+                    <h1>Marketing Material</h1><SubmitButton onClick={handleAddMarketing} className="w-7 h-7 p-1 text-white"><FaPlus className="text-white" size={12} /></SubmitButton>
                 </div>
                 <div className="relative w-[390px] sm:max-w-md">
                     <Input name="email" placeholder="Search Marketing Material" className="h-[42px] border-none shadow-lg rounded-lg font-medium" />
@@ -23,6 +36,7 @@ const MarketingSection = () => {
                     <InfoCard key={index} courseName={marketing} />
                 ))}
             </div>
+            {isAddMarketing && <AddMarketingModal />}
         </div>
     )
 }
--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import courseReducer from "./features/course/courseSlice";
+import marketingReducer from "./features/marketing/marketingSlice"
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    course: courseReducer
+    course: courseReducer,
+    marketing: marketingReducer
 });
 
 export default rootReducer
