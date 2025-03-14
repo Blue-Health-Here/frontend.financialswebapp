345c855928d240e7ca9bbf83579abfa9724effb1
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-03-15 01:11:51 +0500

    add loader whenever api calls
    create a global loader state

--- components/common/Topbar.tsx
+++ components/common/Topbar.tsx
@@ -7,12 +7,14 @@ import { signOutAction } from '@/app/actions'
 import NavbarProfileDropdown from './NavbarProfileDropdown'
 import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineLogout } from "react-icons/md";
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { capitalize } from '@/utils/helperClient'
 import { FaBars } from 'react-icons/fa'
 import { setIsSidebarOpen } from '@/store/features/global/globalSlice'
 import { setUser } from '@/store/features/auth/authSlice'
 import { Session } from '@supabase/supabase-js'
+import { RootState } from '@/store/store'
+import { TbLoader2 } from "react-icons/tb";
 
 interface TopbarProps {
     role?: string;
@@ -21,6 +23,7 @@ interface TopbarProps {
 
 const Topbar: React.FC<TopbarProps> = ({ role, session }) => {
     const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+    const { isLoading } = useSelector((state: RootState) => state.global);
     const dropdownRef = useRef<HTMLDivElement>(null);
     const dispatch = useDispatch();
     const { user } = session ?? {};
@@ -58,34 +61,44 @@ const Topbar: React.FC<TopbarProps> = ({ role, session }) => {
 
     const handleNavbarToggler = () => {
         dispatch(setIsSidebarOpen(true));
-    }
+    };
 
     return (
-        <div className="bg-bodyBG fixed top-0 left-0 lg:left-[250px] xl:left-[300px] right-0 px-6 py-4 z-50">
-            <nav className="topbar bg-white shadow-lg p-4 h-[62px] rounded-lg flex justify-between items-center z-50">
-                <p className="text-[16px] md:text-[18px] lg:text-[20px] xl:text-[21px] font-medium">Tuesday 11, Mar 25</p>
-                <div className="flex justify-end items-center gap-x-4 cursor-pointer">
-                    <div className="relative hidden lg:block">
-                        <IoNotificationsOutline className='w-6 h-6' />
-                        <div className="w-5 h-5 rounded-full bg-primary text-white absolute text-center bottom-3 left-3"><p>8</p></div>
+        <>
+            {isLoading && (
+                <>
+                    <div className="fixed top-0 left-0 right-0 bottom-0 z-[9999] bg-black bg-opacity-75"></div>
+                    <div className="fixed left-0 right-0 top-0 bottom-0 z-[99999] flex items-center justify-center h-full">
+                        <TbLoader2 className='w-10 h-10 text-teal-500 animate-spin' />
                     </div>
-                    <div className="flex gap-x-4 items-center relative" ref={dropdownRef}>
-                        <button className='flex gap-x-3 items-center' onClick={toggleDropdown}>
-                            <div className="text-right hidden md:block">
-                                <span className="text-grey text-sm font-medium">{user?.user_metadata?.name}</span>
-                                <p className="text-themeLight text-sm">{capitalize(role)}</p>
-                            </div>
-                            <Image src={profileImage} alt="" />
-                        </button>
-                        {isDropdownOpen && (
-                            <NavbarProfileDropdown role={role} />
-                        )}
+                </>
+            )}
+            <div className="bg-bodyBG fixed top-0 left-0 lg:left-[250px] xl:left-[300px] right-0 px-6 py-4 z-50">
+                <nav className="topbar bg-white shadow-lg p-4 h-[62px] rounded-lg flex justify-between items-center z-50">
+                    <p className="text-[16px] md:text-[18px] lg:text-[20px] xl:text-[21px] font-medium">Tuesday 11, Mar 25</p>
+                    <div className="flex justify-end items-center gap-x-4 cursor-pointer">
+                        <div className="relative hidden lg:block">
+                            <IoNotificationsOutline className='w-6 h-6' />
+                            <div className="w-5 h-5 rounded-full bg-primary text-white absolute text-center bottom-3 left-3"><p>8</p></div>
+                        </div>
+                        <div className="flex gap-x-4 items-center relative" ref={dropdownRef}>
+                            <button className='flex gap-x-3 items-center' onClick={toggleDropdown}>
+                                <div className="text-right hidden md:block">
+                                    <span className="text-grey text-sm font-medium">{user?.user_metadata?.name}</span>
+                                    <p className="text-themeLight text-sm">{capitalize(role)}</p>
+                                </div>
+                                <Image src={profileImage} alt="" />
+                            </button>
+                            {isDropdownOpen && (
+                                <NavbarProfileDropdown role={role} />
+                            )}
+                        </div>
+                        <FaBars size={22} className='text-primary block lg:hidden' onClick={handleNavbarToggler} />
+                        <MdOutlineLogout onClick={signOutAction} className='w-7 h-7 text-primary' />
                     </div>
-                    <FaBars size={22} className='text-primary block lg:hidden' onClick={handleNavbarToggler} />
-                    <MdOutlineLogout onClick={signOutAction} className='w-7 h-7 text-primary' />
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
+        </>
     )
 }
 
