47d616f658677e51533c5d75381e4379b21fd823
Mehreen47 <mehreen@bluehealthhere.com>
2025-03-24 22:32:26 +0500

    enable download file by clicking on file name

--- components/common/FilePreview.tsx
+++ components/common/FilePreview.tsx
@@ -38,7 +38,7 @@ const FilePreview: React.FC<FilePreviewProps> = ({ file, handleDelete, }) => {
 
     return (
         <div className="flex items-center justify-between bg-white px-4 py-2  border rounded-lg shadow-sm w-full">
-            <span className="text-sm">{file.name}</span>
+            <span className="text-sm cursor-pointer" onClick={fileDownload}>{file.name}</span>
             <div className="flex space-x-3 cursor-pointer">
                 <MdOutlineFileDownload className="text-grey" size={20} onClick={fileDownload} />
                 <Image src="/delete-icon.svg" alt="delete-icon" width={15} height={15} onClick={handleDelete} />
