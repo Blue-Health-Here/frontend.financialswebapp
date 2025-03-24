da51efbb1e4fd1c9425fb50fcd57e8c75396f0a5
Mehreen47 <mehreen@bluehealthhere.com>
2025-03-24 15:38:48 +0500

    add download file functionality in course module

--- components/common/FilePreview.tsx
+++ components/common/FilePreview.tsx
@@ -1,5 +1,7 @@
 "use client"
+import axiosAdmin from "@/lib/axiosAdmin";
 import Image from "next/image";
+import toast from "react-hot-toast";
 import { MdOutlineFileDownload } from 'react-icons/md'
 
 
@@ -11,32 +13,40 @@ interface FilePreviewProps {
 
 const FilePreview: React.FC<FilePreviewProps> = ({ file, handleDelete, }) => {
 
-    const fileDownload = () => {
-        if (!file) {
-            console.error('No file available for download.');
+
+    const fileDownload = async () => {
+        if (!file || !file.file_url) {
+            console.error("No file available for download.");
             return;
         }
-    
+
         try {
-            const blob = new Blob([file]);
+            const response = await axiosAdmin.get(file.file_url, {
+                responseType: "blob",
+            });
+
+            // getting file to avoid format errors
+            const contentType = response.headers["content-type"] || "application/octet-stream";
+
+            const blob = new Blob([response.data], { type: contentType });
             const url = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            console.log("File Content:", file);
-            console.log("File Type:", file.filename);
-            console.log("File url:", file.file_url); 
+            const link = document.createElement("a");
 
             link.href = url;
-            link.download = file.file_url || 'downloaded-file';
+            link.download = file.filename
             document.body.appendChild(link);
             link.click();
-                document.body.removeChild(link);
+            document.body.removeChild(link);
             window.URL.revokeObjectURL(url);
-        } catch (error) {
-            console.error('Error during file download:', error);
+            toast.success("Download file Successfully.");
+        } catch (error: any) {
+            console.error("Error during file download:", error);
+            toast.error(error?.message || "Failed to download file.");
         }
     };
-    
-    
+
+
+
     return (
         <div className="flex items-center justify-between bg-white px-4 py-2  border rounded-lg shadow-sm w-full">
             <span className="text-sm">{file.name}</span>
