073a607a303e87832b0571fa7ee2b76154bd2cdc
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-04-25 22:25:28 +0500

    resolved lint errors

--- components/common/Accordion.tsx
+++ components/common/Accordion.tsx
@@ -6,7 +6,7 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
 interface AccordionProps {
-  items: ChecklistProps[];
+  items: any;
   handleEditQuestion?: () => void;
 }
 
